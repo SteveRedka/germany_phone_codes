f8fb9b84ddd465c339ff2602482c6fdb11e73cf5
Stepan Redka <cogitofallacy@gmail.com>
2019-11-25 22:28:44 +0300

    feat: prefixes matcher for most cases

--- app/queries/phone_codes/prefixes_query.rb
+++ app/queries/phone_codes/prefixes_query.rb
@@ -2,11 +2,40 @@
 
 module PhoneCodes
   class PrefixesQuery
+    VALID_PHONE_REGEX = /(?:\+|\d|\()(?:-|\(|\)|\d|\/|)+/
+    # VALID_PHONE_WITH_POSSIBLE_TRAILING_SPACE = Regexp.new(/^| /.source + VALID_PHONE_REGEX.source)
+    TRAILING_TRASH_REGEX = /^(0049|00|0|\(0|\+49\(0\)|\+49|\+0)/
+    NON_NUMBER_REGEX = /[^\d]/
+
     def self.call(str)
-      terms = str.to_s.downcase.split(' ').join("|")
       PhoneCode.where("""
-        CAST(prefix AS text) ~ ?
-      """, terms)
+        CAST(prefix AS text) IN (?)
+      """, self.list_possible_prefixes(str))
+    end
+
+    def self.list_possible_prefixes(str)
+      str.to_s.split(' ')
+              .map { |s| s.scan(VALID_PHONE_REGEX)  }
+              .flatten
+              .map { |code| self.cleanup_phone_code(code) }
+              .map { |code| self.reduce_prefix(code) }
+              .flatten
+              .uniq
+    end
+
+    private
+
+    def self.cleanup_phone_code(str)
+      str.gsub(TRAILING_TRASH_REGEX, '').gsub(NON_NUMBER_REGEX, '')
+    end
+
+    def self.reduce_prefix(str)
+      # '7932' => ['7932', '793', '79', '7']
+      result = []
+      (0...str.length).each do |i|
+        result << str[0..i]
+      end
+      result.reverse
     end
   end
 end
--- spec/queries/phone_codes/prefixes_query_spec.rb
+++ spec/queries/phone_codes/prefixes_query_spec.rb
@@ -2,20 +2,37 @@ require 'rails_helper'
 
 RSpec.describe PhoneCodes::PrefixesQuery, type: :query do
   let! :phone_code1 { create(:phone_code, prefix: 69, usage: 'Mostly used by fish people') }
+  let! :single_digit_code { create(:phone_code, prefix: 9, usage: 'glob') }
 
   subject { PhoneCodes::PrefixesQuery }
 
-  it 'searches exact prefixes' do
-    expect(subject.call(phone_code1.prefix).first).to eq(phone_code1)
+  describe '#call' do
+    it 'searches exact prefixes' do
+      expect(subject.call(phone_code1.prefix).first).to eq(phone_code1)
+    end
+
+    it 'searches long numbers' do
+      expect(subject.call(695553535).first).to eq(phone_code1)
+    end
+
+    it 'only searches prefixes in beginning of number' do
+      expect(subject.call(111695553).first).to be_nil
+    end
+
+    it 'handles single-digit prefixes' do
+      expect(subject.call(single_digit_code.prefix).first).to eq(single_digit_code)
+    end
   end
 
-  it 'searches long numbers' do
-    expect(subject.call(695553535).first).to eq(phone_code1)
+  describe '#list_possible_prefixes' do
+    it 'handles valid codes' do
+      valid_codes = %w[069 +4969 +49(0)69 004969 (069) 069-123 069/]
+      valid_codes.each do |code|
+        error_msg = "Failing phone code: #{code}"
+        expect(subject.list_possible_prefixes(code)).to include('69', '6'), error_msg
+      end
+    end
+
+    it 'handles whitespaces'
   end
-
-  it 'only searches prefixes in beginning of number'
-
-  it 'works with dashed numbers normally'
-
-  it 'works with whitespaced numbers normally'
 end
