abb037d4d447cfe14725de4c82b798279c5ea39b
Stepan Redka <cogitofallacy@gmail.com>
2019-11-28 09:10:40 +0300

    chore: install jest and ensyme

---
+++ test/javascript/sum.test.js
@@ -0,0 +1,9 @@
+import _ from 'lodash';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+test('1 + 1 equals 2', () => {
+  expect(1 + 1).toBe(2);
+  const asd = <div>asd</div>;
+});
