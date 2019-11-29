b01352a887f7573347529127ea3ee0efd06a8fdd
Stepan Redka <cogitofallacy@gmail.com>
2019-11-29 11:24:09 +0300

    wip

--- app/javascript/components/PhoneCodeList.js
+++ app/javascript/components/PhoneCodeList.js
@@ -8,13 +8,21 @@ class PhoneCodeList extends React.Component {
 
   async loadCodes(e) {
     if(e) { e.preventDefault() };
-    var phoneCodeField = document.getElementById('phone-code-search-input')
-    const response = await fetch(`/api/v1/phone-codes?filter[search]=${phoneCodeField.value}`);
-    const data = await response.json();
-    this.setState({phone_codes: data.data})
+    // var phoneCodeField = document.getElementById('phone-code-search-input')
+    var phoneCodeField = ''
+    console.log('1')
+    try {
+      const response = await fetch(`/api/v1/phone-codes?filter[search]=${phoneCodeField.value}`);
+      // const data = await response.json();
+      // this.setState({phone_codes: data.data})
+      console.log(response)
+    }
+    catch(e) {
+      console.log(e)
+    }
   }
 
-  componentDidMount() {
+  async componentDidMount() {
     this.loadCodes()
   }
 
--- test/javascript/components/PhoneCodeList.test.js
+++ test/javascript/components/PhoneCodeList.test.js
@@ -2,19 +2,28 @@ import React from 'react'
 import {mount, shallow, render, configure} from 'enzyme'
 import PhoneCodeList from 'components/PhoneCodeList.js'
 import Adapter from 'enzyme-adapter-react-16';
-import "isomorphic-fetch"
-
-global.fetch = jest.fn().mockImplementation(() => mockFetchPromise);
+global.fetch = require('jest-fetch-mock')
 
 configure({ adapter: new Adapter() })
 const wrapper = mount(<PhoneCodeList />);
 
 describe('Phone list', function() {
+  beforeEach(() => {
+    fetch.resetMocks()
+  })
+
   test('Renders static table', () => {
     expect(render(wrapper).find('.phone-code-table').length).toBeGreaterThan(0);
   })
 
   test('Lists at least one phone code from database', async () => {
-    // expect(await render(wrapper).find('.phone-code-row').length).toBeGreaterThan(0);
+    var fakeResponse = JSON.stringify({ data: [{id: 1, attributes: { prefix: 'foo'}},
+                                               {id: 2, attributes: { prefix: 'bar'}}]})
+    var fetch = jest.fn()
+    fetch.mockReturnValue(Promise.resolve(fakeResponse));
+
+    var resp = await render(wrapper);
+    expect(fetch.mock.calls.length).toEqual(1)
+    expect(resp.find('.phone-code-row').length).toBeGreaterThan(0);
   })
 })
